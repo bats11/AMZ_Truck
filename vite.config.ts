47c43cf61dadbb3bdd83490f698511d2c49ca427
bats11 <70653789+bats11@users.noreply.github.com>
2025-07-15 17:48:04 +0200

    Revert "compile"
    This reverts commit 959de0b3f8d0c7fa76e86513a54452617a9efdd2.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
-  plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist'
   }
-});
+})
